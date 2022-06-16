cfa18c9a0e33d57af66ef29ce7a775f66b07aadf
Dhia M <merzougui.dhia@gmail.com>
2022-06-16 16:28:30 +0200

    Added initial form

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
-  <h1>Unit Conversion</h1>
+  <div class="app">
+    <h1>Unit Conversion</h1>
+    <ConversionForm />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import ConversionForm from "./components/ConversionForm.vue"
 
 export default defineComponent({
   name: 'App',
-  components: {}
+  components: {
+    ConversionForm
+  }
 });
 </script>
 
---
+++ src/components/ConversionForm.vue
@@ -0,0 +1,55 @@
+<template>
+    <form>
+        <label>Amount</label>
+        <input type="text" v-model.number="amount">
+
+        <label>From:</label>
+        <select v-model="from">
+            <option value="g">Grams</option>
+            <option value="lb">Pounds (lb)</option>
+            <option value="kg">Kilograms</option>
+            <option value="metric_ton">Metric Tons</option>
+        </select>
+        
+        <label>To:</label>
+        <select v-model="to">
+            <option value="g">Grams</option>
+            <option value="lb">Pounds (lb)</option>
+            <option value="kg">Kilograms</option>
+            <option value="metric_ton">Metric Tons</option>
+        </select>
+
+        <button @click="handleClick">Convert</button>
+
+        <h3 id="result" v-if="result">Result: {{result}} {{ to }}</h3>
+    </form>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+    name: "ConversionForm",
+    setup() {
+
+        const amount = ref<number>()
+        const from = ref<string>()
+        const to = ref<string>()
+
+        const handleClick = (e: Event) =>{
+            e.preventDefault()
+        }
+
+        return {
+            amount,
+            from,
+            to,
+            handleClick
+        }
+    },
+})
+</script>
+
+<style>
+
+</style>
